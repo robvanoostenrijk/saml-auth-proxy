9b741a4ad0eb5cedeedd1c1b73aade744378e0eb
Rob van Oostenrijk <robvanoostenrijk@users.noreply.github.com>
2024-12-18 20:22:16 +0400

    SAML_PROXY_AUTH_VERIFY: Return 401 for anonymous sessions

--- server/server.go
+++ server/server.go
@@ -113,14 +113,17 @@ func Start(ctx context.Context, logger *zap.Logger, cfg *Config) error {
 	app := http.HandlerFunc(proxy.handler)
 	http.Handle("/saml/", middleware)
 	http.Handle("/_verify", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		session := samlsp.SessionFromContext(r.Context())
 
-		if IsAnonymousSession(session) {
+		session, err := middleware.Session.GetSession(r)
+
+		if session != nil {
+			w.WriteHeader(200)
+		}
+
+		if err == samlsp.ErrNoSession {
 			w.WriteHeader(401)
 			return
 		}
-
-		w.WriteHeader(200)
 	}))
 	http.Handle("/_health", http.HandlerFunc(proxy.health))
 	http.Handle("/", middleware.RequireAccount(app))
